4fb7fbdd85ddd1f0a64d0bc2099012bc28e0bcc5
felipe camacho <felipe@cogroupsas.com>
2020-01-29 18:20:23 -0500

    update preload with colors theme

--- resources/views/partials/preloader.blade.php
+++ resources/views/partials/preloader.blade.php
@@ -1,4 +1,4 @@
-<div class="preloader">
+<div class="preloader" data-color="{{ config('cogroupcms.color_theme') }}">
 	<div class="loader">
 		<img class="rounded-circle" src="{{ (empty(cms_settings()->favicon)) ? asset('vendor/cogroup/cms/images/favicon.png') : route('getFile', cms_settings()->favicon) }}">
 	</div>
