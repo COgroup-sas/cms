ccefc4dc31b3c2edb7b9a343cf15a72a1b07952a
felipe camacho <felipe@cogroupsas.com>
2020-01-28 23:24:38 -0500

    include libraries

---
+++ public/js/jquery-datatable.js
@@ -0,0 +1,37 @@
+jQuery(function () {
+	if(jQuery('.js-basic').length > 0) {
+    jQuery('.js-basic').DataTable({
+    	resposive: true,
+      "language": {
+        "url": SITE_URL +  "vendor/cogroup/cms/js/i18n/spanish.datatable.json"
+      }
+    });
+  };
+  if(jQuery('.js-exportable').length > 0) {
+    //Exportable table
+    jQuery('.js-exportable').DataTable({
+  		resposive: true,
+      dom: 'Bfrtip',
+      buttons: [
+          'copy', 'csv', 'excel', 'pdf', 'print'
+      ],
+      "language": {
+        "url": SITE_URL +  "vendor/cogroup/cms/js/i18n/spanish.datatable.json"
+      }
+    });
+  };
+  if(jQuery('.from-ajax').length > 0) {
+    //Exportable table
+    jQuery('.from-ajax').DataTable({
+      resposive: true,
+      dom: 'Bfrtip',
+      buttons: [
+          'copy', 'csv', 'excel', 'pdf', 'print'
+      ],
+      "language": {
+        "url": SITE_URL +  "vendor/cogroup/cms/js/i18n/spanish.datatable.json"
+      },
+      ajax: jQuery(this).data('source')
+    });
+  };
+});
